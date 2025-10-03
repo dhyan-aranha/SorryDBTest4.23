848baaed4e6abd0851e8ad26e5770da029ca0685
dhyan-aranha <51273141+dhyan-aranha@users.noreply.github.com>
2025-10-03 13:51:54 +0200

    Comment out PremiseSelection import in TestSorry.lean
    Comment out the import statement for PremiseSelection.

--- SorryDBTest423/TestSorry.lean
+++ SorryDBTest423/TestSorry.lean
@@ -1,4 +1,4 @@
 import Mathlib
-import PremiseSelection
+--import PremiseSelection
 
 lemma zero_imp_factor (g : Polynomial ℤ) (x : ℤ) : g.eval x = 0 ↔ ∃ (h : Polynomial ℤ), g = h * (Polynomial.X - Polynomial.C x) := by sorry
